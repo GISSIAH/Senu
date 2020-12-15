649a766ec6372e0227002b5407d967b6d819a3eb
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-12-14 20:17:55 -0800

    changed dbname
    ehhh

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb://GISSIAH:DizzyAttic99@157.245.88.228:27017/senu?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+dbPassword = 'mongodb://GISSIAH:DizzyAttic99@157.245.88.228:27017/users?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 
 
 module.exports = {
